7b66470cc664cb9035aa463c3b0a7a33ead3561e
Elvis-Guy <guyelvisbakunzi@gmail.com>
2023-07-26 16:46:07 +0000

    added codes

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,8 +1,6 @@
-export default function getListStudentIds(arr) {
-  let newArr = [];
-  if (arr instanceof Array) {
-    newArr = arr.map((item) => item.id);
+export default function getListStudentIds(students) {
+  if (students instanceof Array) {
+    return students.map((student) => student.id);
   }
-
-  return newArr;
+  return [];
 }
